e52c0e4406714fb5fa8db966d8d36d1fa949af50
xryptt <xryptt@gmail.com>
2025-02-11 18:31:46 +0530

    Mobile frontend change

--- frontend/src/components/HomePage.jsx
+++ frontend/src/components/HomePage.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import './index.css';
 import { DotLottieReact } from '@lottiefiles/dotlottie-react';
 import image from '../assets/image.png';
 import portfolio from '../assets/portfolio.svg';
@@ -32,7 +33,7 @@ const LogoSlider = () => {
   ];
 
   return (
-    <div className="relative w-full overflow-hidden bg-background py-10" style={{ width: '90vw', marginLeft: '-5vw' }}>
+    <div id="homepageLogoSlider" className="relative w-full overflow-hidden bg-background py-10" style={{ width: '90vw', marginLeft: '-5vw' }}>
       <div className="flex animate-scroll space-x-16">
         <div className="flex space-x-10 whitespace-nowrap">
           {logos.map((logo, index) => (
@@ -75,17 +76,17 @@ const HomePage = ({ onGetStarted }) => {
     <div>
       <div className="text-center space-y-24">
         <div className="max-w-6xl mx-auto">
-          <div className="flex items-center justify-between">
+          <div id="homepageFlex" className="flex items-center justify-between">
             <div className="text-left max-w-2xl">
-              <h1 style={{ fontSize: '58px' }} className="text-5xl font-extrabold text-primary sm:text-6xl md:text-7xl leading-tight">
+              <h1 id="homepageMainHeading" style={{ fontSize: '58px' }} className="text-5xl font-extrabold text-primary sm:text-6xl md:text-7xl leading-tight">
                 The World's First All-in-One Ethereum Monitoring Solution.
               </h1>
 
-              <p style={{ fontSize: '28px' }} className="mt-6 text-xl text-primary/80 sm:text-2xl md:text-3xl leading-relaxed">
+              <p id="homepageParaText" style={{ fontSize: '28px' }} className="mt-6 text-xl text-primary/80 sm:text-2xl md:text-3xl leading-relaxed">
                 Track wallets, analyze portfolios, and receive alerts for transactions – redefining Ethereum transparency.
               </p>
 
-              <div className="mt-10">
+              <div id="homepageGetStarted" className="mt-10">
                 <button
                   onClick={onGetStarted}
                   className="px-8 py-4 text-lg font-medium text-background bg-primary hover:bg-primary-hover transition-colors duration-200 rounded-lg shadow-lg hover:shadow-xl"
@@ -95,7 +96,7 @@ const HomePage = ({ onGetStarted }) => {
               </div>
             </div>
 
-            <div style={{ marginTop: '-40px', height: '460px', marginLeft: '30px' }} className="w-[500px] h-[500px]">
+            <div id="homepageAnimation" style={{ marginTop: '-40px', height: '460px', marginLeft: '30px' }} className="w-[500px] h-[500px]">
               <DotLottieReact
                 src="https://lottie.host/be3d9bee-4cb7-489a-8cfd-e540b3e07807/7Zd8rmSOND.lottie"
                 loop
@@ -107,7 +108,7 @@ const HomePage = ({ onGetStarted }) => {
 
         <LogoSlider />
 
-        <div style={{ borderRadius: '20px', position: 'relative', width: '89vw' }}>
+        <div id="homepageHero2Image" style={{ borderRadius: '20px', position: 'relative', width: '89vw' }}>
           <img 
             style={{ 
               borderRadius: '20px',
@@ -133,8 +134,8 @@ const HomePage = ({ onGetStarted }) => {
             }}
             className="bg-background-secondary py-16"
           >
-            <div className="max-w-4xl mx-auto">
-              <h2 style={{ fontSize: '40px' }} className="text-3xl font-bold text-primary sm:text-4xl md:text-5xl leading-tight">
+            <div id="homepageHero2" className="max-w-4xl mx-auto">
+              <h2 id="homepageHero2Heading" style={{ fontSize: '40px' }} className="text-3xl font-bold text-primary sm:text-4xl md:text-5xl leading-tight">
                 Stay Ahead of the Game – Monitor Ethereum Wallets and Tokens Seamlessly.
               </h2>
             </div>
@@ -142,38 +143,38 @@ const HomePage = ({ onGetStarted }) => {
         </div>
 
         <div className="max-w-6xl mx-auto px-4 space-y-8">
-          <div className="flex items-center justify-between p-8 border border-primary/10 rounded-xl">
-            <div className="w-1/2 pr-8">
-              <h3 className="text-2xl font-bold text-primary mb-4">Wallet Tracking</h3>
-              <p className="text-lg text-primary/70">
+          <div id="homepageUsecaseBox" className="flex items-center justify-between p-8 border border-primary/10 rounded-xl">
+            <div id="homepageUsecaseContent" className="w-1/2 pr-8">
+              <h3 id="homepageUsecaseHeading" className="text-2xl font-bold text-primary mb-4">Wallet Tracking</h3>
+              <p id="homepageUsecasePara" className="text-lg text-primary/70">
                 Track Whale Wallets and Copy Their Moves. Get real-time notifications when whales make transactions. Copy their moves and make smarter investment decisions.
               </p>
             </div>
-            <div className="w-1/2">
+            <div id="homepageUsecaseImage" className="w-1/2">
               <img src={tracker} style={{ width: '300px', height: '300px', marginLeft: '100px' }} alt="Tracker" />
             </div>
           </div>
 
-          <div className="flex items-center justify-between p-8 border border-primary/10 rounded-xl">
-            <div className="w-1/2">
+          <div id="homepageUsecaseBox" className="flex items-center justify-between p-8 border border-primary/10 rounded-xl">
+            <div id="homepageUsecaseImage" className="w-1/2">
               <img src={tokenAnalysis} style={{ width: '300px', height: '300px', marginLeft: '20px' }} alt="Token Analysis" />
             </div>
-            <div className="w-1/2 pl-8">
-              <h3 className="text-2xl font-bold text-primary mb-4">Identify Short-Term Whales</h3>
-              <p className="text-lg text-primary/70">
+            <div  id="homepageUsecaseContent2" className="w-1/2 pl-8">
+              <h3 id="homepageUsecaseHeading" className="text-2xl font-bold text-primary mb-4">Identify Short-Term Whales</h3>
+              <p id="homepageUsecasePara" className="text-lg text-primary/70">
                 Track recent transactions to find token holders with the largest amounts. Spot potential short-term whales and make timely investment moves.
               </p>
             </div>
           </div>
 
-          <div className="flex items-center justify-between p-8 border border-primary/10 rounded-xl">
-            <div className="w-1/2 pr-8">
-              <h3 className="text-2xl font-bold text-primary mb-4">View Wallet Portfolio</h3>
-              <p className="text-lg text-primary/70">
+          <div id="homepageUsecaseBox" className="flex items-center justify-between p-8 border border-primary/10 rounded-xl">
+            <div id="homepageUsecaseContent" className="w-1/2 pr-8">
+              <h3 id="homepageUsecaseHeading" className="text-2xl font-bold text-primary mb-4">View Wallet Portfolio</h3>
+              <p id="homepageUsecasePara" className="text-lg text-primary/70">
                 Get a detailed view of any wallet's portfolio. Analyze assets, track holdings, and understand the investment strategy behind a wallet address.
               </p>
             </div>
-            <div className="w-1/2">
+            <div id="homepageUsecaseImage" className="w-1/2">
               <img src={portfolio} style={{ width: '300px', height: '300px', marginLeft: '100px' }} alt="Portfolio" />
             </div>
           </div>
